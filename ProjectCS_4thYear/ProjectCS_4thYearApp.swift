d6715c2ddd414c05dd0405d7163de52691405c90
Freddy Sourial <freddy.sourial@gmail.com>
2023-03-04 03:15:32 -0500

    Initial Commit

---
+++ ProjectCS_4thYear/ProjectCS_4thYearApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectCS_4thYearApp.swift
+//  ProjectCS_4thYear
+//
+//  Created by Freddy Sourial on 2023-03-04.
+//
+
+import SwiftUI
+
+@main
+struct ProjectCS_4thYearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
