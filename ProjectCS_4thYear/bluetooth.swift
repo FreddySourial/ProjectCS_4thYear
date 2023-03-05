d362a1baa13d65243b8c0edd62f97f84831344f7
Freddy Sourial <freddy.sourial@gmail.com>
2023-03-05 02:00:57 -0500

    not much updated.

--- ProjectCS_4thYear/bluetooth.swift
+++ ProjectCS_4thYear/bluetooth.swift
@@ -23,31 +23,58 @@ class BluetoothViewModel: NSObject, ObservableObject {
 }
 extension BluetoothViewModel: CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == .poweredOn {
-            self.centralManager?.scanForPeripherals(withServices: nil)
+     var n=2
+        while (n<=10) {
+            if central.state == .poweredOn {
+                self.centralManager?.scanForPeripherals(withServices: nil)
+            n+=2
+            }
         }
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         
+      var n=2
+        if (n<=10) {
+            
         
-        if !peripherals.contains(peripheral) {
-            self.peripherals.append(peripheral)
-            self.peripheralNames.append(peripheral.name ?? "unnamed device")
-            print("Peripheral name: \(peripheral.name)")
-            
-            
-            if (peripheral.name=="MX Master"){
-                bleconnect = true
-                bleconnect1 = true
-                print("Connected OH BABBBYYYYY")
-                print (bleconnect)
+        
+            if !peripherals.contains(peripheral) {
+                self.peripherals.append(peripheral)
+                self.peripheralNames.append(peripheral.name ?? "unnamed device")
+                print("Peripheral name: \(peripheral.name)")
                 
-//                "Freddy’s MacBook Pro"
                 
+                
+                
+            
+                if (peripheral.name=="MX Master"){
+                    bleconnect = true
+                    bleconnect1 = true
+                    print("Connected OH BABBBYYYYY")
+                    print (bleconnect1)
+                    
+                    //                "Freddy’s MacBook Pro"
+                    
+                } else if (peripheral.name != "MX Master") {
+                    bleconnect1 = false
+                    print("NO CONNECT")
+                    print (bleconnect1)
+                }
+                //            else if (peripheral.name != "Freddy’s iPad") {
+                //                    bleconnect1 = false
+                //                    print("NO CONNECT")
+                //                    print (bleconnect1)
+                //
+                //                }
+                
+                
+                n+=2
+                print(n)
             }
-            
+        
         }
+        
     }
 }
 
@@ -64,30 +91,63 @@ struct bluetooth: View {
     @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
     
     var body: some View {
-        VStack{
-            
+        
+       VStack{
+            Spacer()
+           
+           
+//           Button (action: { function()
+//           }, label: {
+//               Text("Text")
+//           })
+
             if bluetoothViewModel.bleconnect1 == true {
-                Text("Daddyy")
+                
+                Text("Bluetooth Connection is Established. Device is nearby.")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    
+                    .padding(.all)
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 100.0)
+                        .background(Color.green)
+                        .cornerRadius(10)
             } else {
-                Text ("BOOOO")
+                Text ("BLUETOOTH CONNECTION HAS BEEN LOST! SEARCHING FOR GPS COORDINATES")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    
+                    .padding(.all)
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 100.0)
+                        .background(Color.red)
+                        .cornerRadius(10)
             }
             Text("test \(bluetoothViewModel.bleconnect1)" as String)
             Toggle ("Is Bluetooth connected?" , isOn: $togglebutton)
             
             Text("Peripherals")
             NavigationView {
+                
                 List(bluetoothViewModel.peripheralNames, id: \.self) { peripheral in
                     Text(peripheral)
+                } .refreshable {
+                    
                 }
-                //            .navigationTitle("Peripherals") .padding(.top, 0.0)
+//                List(bluetoothViewModel.peripheralNames, id: \.self) { peripheral in
+//                    Text(peripheral)
+//                }
+                
            
             }
         }
+        
         
     }
 }
 struct bluetooth_Previews: PreviewProvider {
     static var previews: some View {
+        
         bluetooth()
     }
 }
