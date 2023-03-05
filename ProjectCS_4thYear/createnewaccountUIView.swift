ef2ee9a6aeeacb323c1521a6facf37066eb65992
Freddy Sourial <freddy.sourial@gmail.com>
2023-03-04 23:14:59 -0500

    "Create new account view" updated!

--- ProjectCS_4thYear/createnewaccountUIView.swift
+++ ProjectCS_4thYear/createnewaccountUIView.swift
@@ -12,35 +12,106 @@ struct createnewaccountUIView: View {
     
     @State var password1 = ""
     @State var username1 = ""
+    @State var showingalert: Bool = false
+    @State var showingalert1: Bool = false
+    @State private var text: String = ""
+    @State private var color: Color = Color.gray
+    @State private var color2: Color = Color.gray
+    @State private var count: Int = 0
+    @State private var count2: Int = 0
+    
     var body: some View {
         
         VStack {
            
-            VStack(spacing: 5){
-                Text("Create New Account")
+            VStack(spacing: 11.0){
+                Text("Create New Account ")
                     .font(.title)
                     .fontWeight(.bold)
                     .multilineTextAlignment(.center)
-                TextField ("username1", text: $username1)
+                HStack {
+                    TextField ("username1 ", text: $username1) .overlay(RoundedRectangle(cornerRadius:5) .stroke(color,lineWidth:1))
+                        .foregroundColor(color)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.all)
+                        .onChange(of: username1) { text in
+                            let letters = text.trimmingCharacters(in: .whitespaces).count
+                            self.count = letters
+                            
+                            switch letters {
+                            case 1..<5:
+                                self.color = .red
+                            case 5..<8:
+                                self.color = .orange
+                            case 8...:
+                                self.color = .green
+                            default:
+                                self.color = .gray
+                                
+                            }
+                        }
+
+//                    Text("\(count)")
+                }
+               
+                TextField("password1", text: $password1) .overlay(RoundedRectangle(cornerRadius:5) .stroke(color2,lineWidth:1))
+                    .foregroundColor(color2)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding([.top, .leading, .bottom])
-                TextField("password1", text: $password1)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding([.top, .leading, .bottom])
+                    .padding(.all)
+                    .onChange(of: password1) { text1 in
+                        let letters2 = text1.trimmingCharacters(in: .whitespaces).count
+                        self.count2 = letters2
+                        
+                        switch letters2{
+                        case 1..<5:
+                            self.color2 = .red
+                        case 5..<8:
+                            self.color2 = .orange
+                        case 8...:
+                            self.color2 = .green
+                        default:
+                            self.color2 = .gray
+                            
+                        }
+                    }
                 Button(action: {
-                    model.addUser(username1: username1, password1: password1)
-                    username1 = ""
-                    password1 = ""
+                    
+//                    model.addUser(username1: username1, password1: password1)
+//                    username1 = ""
+//                    password1 = ""
+                    showingalert = true
                 }, label: {
                     Text("Create New Account")
                 })
-                .padding([.top, .leading, .bottom])
+                .padding(.all)
                 .foregroundColor(.white)
                     .frame(width: 300, height: 50)
                     .background(Color.blue)
                     .cornerRadius(10)
-            }
-        }
+                
+                    .alert(isPresented: $showingalert){
+                        Alert(
+                            title: Text("Create New Account?"),
+                    message: Text ("Are you sure you want to create a new account?"),
+                            primaryButton: .default(Text("Yes")){
+                                model.addUser(username1: username1, password1: password1)
+                                username1 = ""
+                                password1 = ""
+                                showingalert1 = true
+                            },
+                            secondaryButton: .cancel(){}
+                            )
+                    }
+                
+                
+                    .alert("Congrats! Your new account has been created! Please go back and login!", isPresented: $showingalert1, actions:{})
+//                        Alert(
+//                            title: Text("Congrats! "),
+//                    message: Text ("Your new account has been created! Please go back and Login"))
+                    
+                    .padding()
+            }.padding(.all) .overlay(RoundedRectangle(cornerRadius:5) .stroke(.black,lineWidth:2))
+        } .padding(.all, 20.0)
     }
 }
 
