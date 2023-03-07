4b1b13afe378aab28f1ae1fabee5060c86cd1645
Freddy Sourial <freddy.sourial@gmail.com>
2023-03-06 23:29:08 -0500

    firebaseAuth

---
+++ ProjectCS_4thYear/logintest.swift
@@ -0,0 +1,20 @@
+//
+//  logintest.swift
+//  ProjectCS_4thYear
+//
+//  Created by Freddy Sourial on 2023-03-06.
+//
+
+import SwiftUI
+
+struct logintest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct logintest_Previews: PreviewProvider {
+    static var previews: some View {
+        logintest()
+    }
+}
